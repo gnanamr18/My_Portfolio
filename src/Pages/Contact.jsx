4e3e116c7706b1399f0d46b1a14dcfd0e4bd0c63
GnanamRajagopalan <gnanam.r18@gmail.com>
2024-11-24 04:16:00 +0530

    working in contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,11 +3,9 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Icon from "../components/Icon";
 import Content from "../components/Content";
-import textContent from "../data/textContent.js"
-
+import textContent from "../data/textContent.js";
 
 const Contact = ({ handleOnClick, selectedItem }) => {
-
   return (
     <div className="bg-gray-200 text-gray-800 min-h-screen antialiased flex flex-col w-fit sm:w-full ">
       {/* Navbar */}
@@ -16,15 +14,29 @@ const Contact = ({ handleOnClick, selectedItem }) => {
       </div>
       {/* Contact page */}
       <Content>
-        <section  className="flex flex-col items-center md:flex-row md:justify-center gap-20">
+        <section className="flex flex-col items-center md:flex-row md:justify-center gap-20">
           {/* Bold Text */}
           <div className="flex flex-col gap-8  mt-5">
             <span className="font-serif font-semibold text-2xl leading-loose  md:text-5xl md:leading-loose md:max-w-5xl">
               {textContent.contact.intro}
             </span>
             {/* Normal Text */}
-            <p className="font-mono text-xl mt-2 leading-loose  p-1 ">
-              {textContent.contact.description}
+            <p className="font-mono text-xl mt-2 leading-loose p-1">
+              {textContent.contact.description1} If you have a specific question
+              or comment, please feel free to email me directly at{" "}
+              <span className="bg-yellow-500">
+                {" "}
+                {textContent.contact.email}
+              </span>
+              {textContent.contact.description2}
+              <a
+                href={textContent.contact.linkedin}
+                target="_blank"
+                className="text-blue-600 underline"
+              >
+                {textContent.contact.linkedin}
+              </a>
+             {textContent.contact.description3}
             </p>
           </div>
         </section>
--- src/data/textContent.js
+++ src/data/textContent.js
@@ -17,25 +17,17 @@ const textContent = {
   },
   contact: {
     intro: "Let's Get in Touch: Ways to Connect with Me",
-    description: `Thank you for your interest in getting in touch with me. I welcome
-                    your feedback, questions, and suggestions. If you have a specific
-                    question or comment, please feel free to email me directly at
-                    <span className="bg-yellow-500"> gnanam.r18@gmail.com</span>. I
-                    make an effort to respond to all messages within 24 hours,
-                    although it may take me longer during busy periods. Alternatively,
-                    you can use the contact form on my website to get in touch. Simply
-                    fill out the required fields and I'll get back to you as soon as
-                    possible. Finally, if you prefer to connect on social media, you
-                    can find me on
-                    <a
-                      href="https://www.linkedin.com/in/gnanam-r/"
-                      target="_blank"
-                      className="text-blue-600 underline"
-                    >
-                      {" "}
-                      https://www.linkedin.com/in/gnanam-r/
-                    </a>
-                    . I post regular updates and engage with my followers there, so
+    email: "gnanam.r18@gmail.com",
+    linkedin: "https://www.linkedin.com/in/gnanam-r/",
+    description1: `Thank you for your interest in getting in touch with me. I welcome
+                  your feedback, questions, and suggestions.`,
+    description2: `. I make an effort to respond to all messages within 24 hours,
+                  although it may take me longer during busy periods. Alternatively,
+                   you can use the contact form on my website to get in touch. Simply
+                  fill out the required fields and I'll get back to you as soon as
+                  possible. Finally, if you prefer to connect on social media, you
+                  can find me on{" "}`,
+    description3: ` . I post regular updates and engage with my followers there, so
                     don't hesitate to reach out. Thanks again for your interest, and I
                     look forward to hearing from you!`,
   },
